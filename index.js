91140ee594d94b70e8e397f3760ffd5eae894c4e
Eduardo <dudubor2003@gmail.com>
2025-01-08 09:59:32 -0300

    Fix cookies error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('ffmpeg-static');
+const sanitize = require('sanitize-filename');
 require('dotenv').config();
 
 // Configura o caminho do ffmpeg
@@ -15,6 +16,7 @@ const client = new Client({
 
 // Seu token do Discord
 const TOKEN = process.env.DISCORD_TOKEN;
+
 client.on('ready', () => {
     console.log(`Bot logado como ${client.user.tag}`);
 });
@@ -36,20 +38,31 @@ client.on('messageCreate', async (message) => {
                 fs.mkdirSync(downloadsDir);
             }
 
+            const cookies = fs.readFileSync('./cookies.txt', 'utf8')
+
             // Baixa o áudio do YouTube
             const info = await ytdl.getInfo(url);
-            const audioStream = ytdl(url, { filter: 'audioonly' });
+            const audioStream = ytdl(url, {
+                filter: 'audioonly',
+                highWaterMark: 1 << 25,
+                requestOptions: {
+                    Headers: {
+                        'Cookie': cookies,
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                    }
+                }
+            });
 
-            const sanitizedTitle = info.videoDetails.title
-            .replace(/[<>:"/\\|?*]+/g, '') 
-            .replace(/\s+/g, '_'); 
-
+            const safeFileName = sanitize(info.videoDetails.title)
+            
             // Define o nome do arquivo
-            const fileName = `${downloadsDir}/${sanitizedTitle}.mp3`;
+            const fileName = `${downloadsDir}/${safeFileName}.mp3`;
             // Converte para MP3 usando ffmpeg
             ffmpeg(audioStream)
+                .audioCodec('libmp3lame')
                 .audioBitrate(128)
                 .toFormat('mp3')
+                .output(fileName)
                 .save(fileName)
                 .on('end', () => {
                     message.reply('Download concluído! Aqui está o arquivo:');
